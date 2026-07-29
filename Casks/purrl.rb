42ef73024ef3af25cc3de7ad7a977d7f437783cd
rbc33 <ricardobenthem@gmail.com>
2026-07-29 16:35:34 +0200

    v1.0.3

--- Casks/purrl.rb
+++ Casks/purrl.rb
@@ -1,6 +1,6 @@
 cask "purrl" do
-  version "1.0.2"
-  sha256 "ebaae08f4b7141c0d2265a13d5a45b0795cdfef29f6cd6ef1bd0b9efcc2d01ee"
+  version "1.0.3"
+  sha256 "aa8f38b1c515d88def1a70624b73d7506aeac1815692626304c6c2559cf065a9"
 
   url "https://github.com/rbc33/Purrl/releases/download/v#{version}/Purrl.dmg"
   name "Purrl"
